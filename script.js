1da3338001bdbf46e5315afefb16e656175e15bd
giahan-hust <giahan2594@gmail.com>
2025-03-04 19:56:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="giahan" && p=="2202")
+  {
+    window.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
